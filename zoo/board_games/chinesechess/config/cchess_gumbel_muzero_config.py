74681450849e17db05933dc4dac1a9cda012ff55
mycve <mycve@qq.com>
2025-12-31 14:07:28 +0800

    优化规则

--- zoo/board_games/chinesechess/config/cchess_gumbel_muzero_config.py
+++ zoo/board_games/chinesechess/config/cchess_gumbel_muzero_config.py
@@ -21,8 +21,8 @@ from zoo.board_games.chinesechess.envs.action_mapping import ACTION_SPACE_SIZE
 # ==============================================================
 # 常用配置参数（用户可修改区域）
 # ==============================================================
-collector_env_num = 8
-n_episode = 8
+collector_env_num = 64
+n_episode = 128
 evaluator_env_num = 3
 num_simulations = 50
 update_per_collect = 50
